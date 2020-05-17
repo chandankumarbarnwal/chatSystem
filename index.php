a9074f0877028a65e75ac6ea29e9c788a72b9546
chandankumarbarnwal <chandanbarnwal111@gmail.com>
2020-05-17 12:50:53 +0530

    Send image in group chat

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
-<!--
-//index.php
-!-->
-
 <?php
 
 include('database_connection.php');
 
 session_start();
-
-
-// echo $_SESSION['username'];
-// return;
 
 if(!isset($_SESSION['user_id']))
 {
@@ -28,6 +20,7 @@ if(!isset($_SESSION['user_id']))
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="https://cdn.rawgit.com/mervick/emojionearea/master/dist/emojionearea.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.js"></script>
     </head>  
     <body>  
         <div class="container">
@@ -35,37 +28,90 @@ if(!isset($_SESSION['user_id']))
    
    <h3 align="center">Chat Application using PHP Ajax Jquery</a></h3><br />
    <br />
-   
+   <div class="row">
+    <div class="col-md-8 col-sm-6">
+     <h4>Online User</h4>
+    </div>
+    <div class="col-md-2 col-sm-3">
+     <input type="hidden" id="is_active_group_chat_window" value="no" />
+     <button type="button" name="group_chat" id="group_chat" class="btn btn-success left">Group Chat</button>
+    </div>
+    <div class="col-md-2 col-sm-3">
+     <p align="right">Hi - <?php echo $_SESSION['username']; ?> - <a href="logout.php">Logout</a></p>
+    </div>
+   </div>
    <div class="table-responsive">
-    <h4 align="center">Online User</h4>
-
-    <input type="hidden" id="is_active_group_chat_window" value="no" />
-<button type="button" name="group_chat" id="group_chat" class="btn btn-success btn-xs">Group Chat</button>
-
-
-
-    <p align="right">Hi - <?php echo $_SESSION['username'];  ?> - <a href="logout.php">Logout</a></p>
+    
     <div id="user_details"></div>
     <div id="user_model_details"></div>
    </div>
   </div>
     </body>  
-</html>  
+</html>
 
+<style>
 
+.chat_message_area
+{
+ position: relative;
+ width: 100%;
+ height: auto;
+ background-color: #FFF;
+    border: 1px solid #CCC;
+    border-radius: 3px;
+}
+
+#group_chat_message
+{
+ width: 100%;
+ height: auto;
+ min-height: 80px;
+ overflow: auto;
+ padding:6px 24px 6px 12px;
+}
+
+.image_upload
+{
+ position: absolute;
+ top:3px;
+ right:3px;
+}
+.image_upload > form > input
+{
+    display: none;
+}
+
+.image_upload img
+{
+    width: 24px;
+    cursor: pointer;
+}
+
+</style>  
 
 <div id="group_chat_dialog" title="Group Chat Window">
  <div id="group_chat_history" style="height:400px; border:1px solid #ccc; overflow-y: scroll; margin-bottom:24px; padding:16px;">
 
  </div>
  <div class="form-group">
-  <textarea name="group_chat_message" id="group_chat_message" class="form-control"></textarea>
+  <!--<textarea name="group_chat_message" id="group_chat_message" class="form-control"></textarea>!-->
+  <div class="chat_message_area">
+   <div id="group_chat_message" contenteditable class="form-control">
+
+   </div>
+   <div class="image_upload">
+    <form id="uploadImage" method="post" action="upload.php">
+     <label for="uploadFile"><img src="image3.jpeg" alt="I">
+</label>
+     <input type="file" name="uploadFile" id="uploadFile" />
+    </form>
+   </div>
+  </div>
  </div>
  <div class="form-group" align="right">
   <button type="button" name="send_group_chat" id="send_group_chat" class="btn btn-info">Send</button>
  </div>
 </div>
-
 
 
 <script>  
@@ -169,6 +215,7 @@ $(document).ready(function(){
 
  $(document).on('click', '.ui-button-icon', function(){
   $('.user_dialog').dialog('destroy').remove();
+  $('#is_active_group_chat_window').val('no');
  });
 
  $(document).on('focus', '.chat_message', function(){
@@ -197,72 +244,58 @@ $(document).ready(function(){
   })
  });
 
+ $('#group_chat_dialog').dialog({
+  autoOpen:false,
+  width:400
+ });
 
-$('#group_chat_dialog').dialog({
- autoOpen:false,
- width:400
-});
+ $('#group_chat').click(function(){
+  $('#group_chat_dialog').dialog('open');
+  $('#is_active_group_chat_window').val('yes');
+  fetch_group_chat_history();
+ });
 
+ $('#send_group_chat').click(function(){
+  var chat_message = $('#group_chat_message').html();
+  var action = 'insert_data';
+  if(chat_message != '')
+  {
+   $.ajax({
+    url:"group_chat.php",
+    method:"POST",
+    data:{chat_message:chat_message, action:action},
+    success:function(data){
+     $('#group_chat_message').html('');
+     $('#group_chat_history').html(data);
+    }
+   })
+  }
+ });
 
-
-$('#group_chat').click(function(){
-
-  console.log('group_chat');
-
- $('#group_chat_dialog').dialog('open');
- $('#is_active_group_chat_window').val('yes');
- fetch_group_chat_history();
-});
-
-
-
-$('#send_group_chat').click(function(){
- var chat_message = $('#group_chat_message').val();
- var action = 'insert_data';
- if(chat_message != '')
+ function fetch_group_chat_history()
  {
-  $.ajax({
-   url:"group_chat.php",
-   method:"POST",
-   data:{chat_message:chat_message, action:action},
-   success:function(data){
-    console.log('insert_data');
-console.log(data);
-
-
-    $('#group_chat_message').val('');
-    $('#group_chat_history').html(data);
-   }
-  })
+  var group_chat_dialog_active = $('#is_active_group_chat_window').val();
+  var action = "fetch_data";
+  if(group_chat_dialog_active == 'yes')
+  {
+   $.ajax({
+    url:"group_chat.php",
+    method:"POST",
+    data:{action:action},
+    success:function(data)
+    {
+     $('#group_chat_history').html(data);
+    }
+   })
+  }
  }
-});
 
-
-
-function fetch_group_chat_history()
-{
- var group_chat_dialog_active = $('#is_active_group_chat_window').val();
- var action = "fetch_data";
- if(group_chat_dialog_active == 'yes')
- {
-  $.ajax({
-   url:"group_chat.php",
-   method:"POST",
-   data:{action:action},
-   success:function(data)
-   {
-
-      console.log('fetch_data');
-console.log(data);
-    $('#group_chat_history').html(data);
-   }
-  })
- }
-}
-
-
-
-
+ $('#uploadFile').on('change', function(){
+  $('#uploadImage').ajaxSubmit({
+   target: "#group_chat_message",
+   resetForm: true
+  });
+ });
  
 });  
 </script>
